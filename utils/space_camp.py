434c8a0fbbd1f2533cd22c941e3b82a54b22d4f1
chanceygardener <ianbicket@gmail.com>
2019-12-07 17:04:16 -0800

    added utils folder for NLU intents for ontological relationships

---
+++ utils/space_camp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import json
+import sys
+import csv
+
+# This script should take a 2 column csv including
+# columns for: orthography NER tag (It can include more)
+# And return a 
